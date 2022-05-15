b9664a41ac2938a3236f007b218bcb8055e397d0
shinjimjim <erics6lihin8em9oics@gmail.com>
2022-05-16 04:59:23 +0900

    トップページの表示

---
+++ db/migrate/20220515181215_create_items.rb
@@ -0,0 +1,15 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.string     :name         , null: false
+      t.text       :content      , null: false
+      t.integer    :category_id  , null: false
+      t.integer    :condition_id , null: false
+      t.integer    :burden_id    , null: false
+      t.integer    :prefecture_id, null: false
+      t.integer    :days_id      , null: false
+      t.integer    :price        , null: false
+      t.timestamps
+    end
+  end
+end
