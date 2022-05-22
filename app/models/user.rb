0fc24c808831472c98c3e38b941f02a6a1423e80
shinjimjim <erics6lihin8em9oics@gmail.com>
2022-05-22 19:21:33 +0900

    (devise)バリデーションとエラーメッセージの表示(修正3)

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-    validates :password,          format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "is invalid. Input half-width alphanumerical characters."}
+    validates :password,          format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "is invalid. Input half-width alphanumerical characters.", allow_blank: true}
  
    with_options presence: true do
     validates :nickname
