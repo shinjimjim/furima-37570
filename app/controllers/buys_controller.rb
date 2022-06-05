edc8e58a3ac2828f3d42b564a5eb94e84b41c002
shinjimjim <erics6lihin8em9oics@gmail.com>
2022-06-05 15:11:46 +0900

    購入画面の情報を保存できるようにする

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -1,6 +1,23 @@
 class BuysController < ApplicationController
   def index
+    @item = Item.find(params[:item_id])
     @buy_address = BuyAddress.new
   end
 
+  def create
+    @item = Item.find(params[:item_id])
+    @buy_address = BuyAddress.new(buy_params)
+    if @buy_address.valid?
+      @buy_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def buy_params
+    params.require(:buy_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :telephone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
 end
