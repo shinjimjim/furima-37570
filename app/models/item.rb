e21978c593000796ac2f97fb1a3b31d61dcee1ef
shinjimjim <erics6lihin8em9oics@gmail.com>
2022-09-13 10:54:49 +0900

    エラーメッセージの日本語化

--- app/models/item.rb
+++ app/models/item.rb
@@ -14,9 +14,9 @@ class Item < ApplicationRecord
   validates :images, :name, :content, presence: true
   validates :images, length: { minimum: 1, maximum: 5, message: "は1枚以上5枚以下にしてください" }
   validates :category_id, :condition_id, :burden_id, :prefecture_id, :delivery_day_id,
-            numericality: { other_than: 1, message: "can't be blank" }
+            numericality: { other_than: 1, message: "を入力して下さい" }
   with_options presence: true, format: { with: /\A[0-9]+\z/ } do
     validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
-                      presence: { message: "can't be blank" }
+                      presence: { message: "を入力して下さい" }
   end
 end
